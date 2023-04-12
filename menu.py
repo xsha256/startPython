72dec4347f890161effef578808aab3d00faa8ff
xsha256 <43494720+xsha256@users.noreply.github.com>
2023-04-12 12:30:41 +0200

    eliminado un menu y modificado el Ej 14

--- menu.py
+++ menu.py
@@ -1,7 +1,6 @@
+from prettytable import PrettyTable
 from termcolor import colored
-
 import os
-#Moha
 from menu.ej1LetraDNI import * 
 from menu.ej2Salario import * 
 from menu.ej3RutaAvion import * 
@@ -12,7 +11,7 @@ from menu import ej6CelAFah
 from menu import ej7ParImpar
 from menu import ej8BisiestoONo
 from menu import ej9Palíndromo
-from menu import ej10Ordenar    
+from menu import ej10Ordenar
 from menu import ej11Factorial
 from menu import ej12Primo
 from menu import ej13Cubo
@@ -25,37 +24,38 @@ from menu import ej18Anagrama
 from menu import ej19Duplicados
 from menu import ej20Capicua
 
-
-
 while True:
     os.system('clear')
-    print(colored("Menú:", "blue"))
-    print(colored("1. Calcular la letra del DNI Español", "green"))
-    print(colored("2. Calcular el salario de un empleado", "red"))
-    print(colored("3. Determinar la ruta para llegar a una ciudad por avión. [Con base de datos]", "white"))
-    print(colored("4. Calcular el área y perímetro de un círculo dado su radio", "blue"))
-    print(colored("5. Dada una lista de números enteros, determinar cuál es el mayor y cuál es el menor", "red"))
-    print(colored("6. Convertir grados Celsius a Fahrenheit", "green"))
-    print(colored("7. Determinar si un número entero es par o impar", "yellow"))
-    print(colored("8. Determinar si un año es bisiesto o no", "white"))
-    print(colored("9. Determinar si una cadena de texto es un palíndromo o no", "red"))
-    print(colored("10. Ordenar alfabéticamente una lista de nombres", "blue"))
-    print(colored("11. Calcular el factorial de un número entero", "yellow"))
-    print(colored("12. Determinar si un número entero es primo o no", "grey"))
-    print(colored("13. Calcular el área y volumen de un cubo dado su lado", "magenta"))
-    print(colored("14. Calcular la suma de todos los números pares de una lista de números enteros", "cyan"))
-    print(colored("15. Determinar si un número es positivo, negativo o cero", "red"))
-    print(colored("16. Calcular la media de una lista de números enteros", "blue"))
-    print(colored("17. Juego: adivinar un número aleatorio entre 1 y 100", "white"))
-    print(colored("18. Determinar si una cadena de texto es un anagrama de otra cadena de texto", "green"))
-    print(colored("19. Eliminar los números duplicados de una lista de números enteros", "yellow"))
-    print(colored("20. Determinar si un número es capicúa o no", "red"))
+    tabla = PrettyTable()
+    tabla.field_names = ["#", "Descripción"]
+    tabla.align["Descripción"] = "l"
+    tabla.add_row([colored("1", "blue"), colored("Calcular la letra del DNI Español", "green")])
+    tabla.add_row([colored("2", "blue"), colored("Calcular el salario de un empleado", "red")])
+    tabla.add_row([colored("3", "blue"), colored("Determinar la ruta para llegar a una ciudad por avión. [Con base de datos]", "white")])
+    tabla.add_row([colored("4", "blue"), colored("Calcular el área y perímetro de un círculo dado su radio", "blue")])
+    tabla.add_row([colored("5", "blue"), colored("Dada una lista de números enteros, determinar cuál es el mayor y cuál es el menor", "red")])
+    tabla.add_row([colored("6", "blue"), colored("Convertir grados Celsius a Fahrenheit", "green")])
+    tabla.add_row([colored("7", "blue"), colored("Determinar si un número entero es par o impar", "yellow")])
+    tabla.add_row([colored("8", "blue"), colored("Determinar si un año es bisiesto o no", "white")])
+    tabla.add_row([colored("9", "blue"), colored("Determinar si una cadena de texto es un palíndromo o no", "red")])
+    tabla.add_row([colored("10", "blue"), colored("Ordenar alfabéticamente una lista de nombres", "blue")])
+    tabla.add_row([colored("11", "blue"), colored("Calcular el factorial de un número entero", "yellow")])
+    tabla.add_row([colored("12", "blue"), colored("Determinar si un número entero es primo o no", "grey")])
+    tabla.add_row([colored("13", "blue"), colored("Calcular el área y volumen de un cubo dado su lado", "magenta")])
+    tabla.add_row([colored("14", "blue"), colored("Calcular la suma de todos los números pares de una lista de números enteros", "cyan")])
+    tabla.add_row([colored("15", "blue"), colored("Determinar si un número es positivo, negativo o cero", "red")])
+    tabla.add_row([colored("16", "blue"), colored("Calcular la media de una lista de números enteros", "blue")])
+    tabla.add_row([colored("17", "blue"), colored("Juego: adivinar un número aleatorio entre 1 y 100", "white")])
+    tabla.add_row([colored("18", "blue"), colored("Determinar si una cadena de texto es un anagrama de otra cadena de texto", "green")])
+    tabla.add_row([colored("19", "blue"), colored("Eliminar los números duplicados de una lista de números enteros", "yellow")])
+    tabla.add_row([colored("20", "blue"), colored("Determinar si un número es capicúa o no", "red")])
+
+    print(colored(tabla.get_string(), "blue"))
     opcion = input(colored("Seleccione una opción (1-20) o escriba '0' para cerrar el programa: ", "blue"))
 
     if opcion == "0":
         print("¡Hasta luego!")
         break
-    #Moha 1/2/3/4/5
     elif opcion == "1":
         print(LetraDNI())
     elif opcion == "2":
@@ -66,7 +66,7 @@ while True:
         print(CalcularAreaPer())
     elif opcion == "5":
         print(CalcularMayMen())
-    # Jose Javier 6/7/8/9/10/11/12/13/14
+            # Jose Javier 6/7/8/9/10/11/12/13/14
     elif opcion == "6":
         print(ej6CelAFah.celAfah())
     elif opcion == "7":
@@ -85,7 +85,7 @@ while True:
         print(ej13Cubo.CalCubo())
     elif opcion == "14":
         print(ej14Suma.CalNumPares())
-    # Jorge 15/16/17/18/19/20
+     # Jorge 15/16/17/18/19/20
     elif opcion == "15":
         ej15DeterminaNum.determinaNum()
     elif opcion == "16":
@@ -98,5 +98,4 @@ while True:
         ej19Duplicados.Duplicados()
     elif opcion == "20":
         ej20Capicua.Capicua()
-
         
--- menu/ej14Suma.py
+++ menu/ej14Suma.py
@@ -2,13 +2,20 @@ def CalNumPares():
 
     op = "si"
     while op == "si":
-        numeros = input("Introduce una lista de números separados por espacios: ").split()
+        numeros = input(
+            "Introduce una lista de números separados por espacios: ").split()
         pares = 0
         for num in numeros:
-            if int(num) % 2 == 0:
-                pares = pares + int(num)
+            if num.isdigit():
+                if int(num) % 2 == 0:
+                    pares = pares + int(num)
+
+            else:
+                print("Error: al menos uno de los valores introducidos no es número")
         print(f"La suma de todos los numeros pares: {pares}")
-        choose = input("Quieres introducir otra lista de numeros? 'si' o 'no': ")
+        choose = input(
+            "Quieres introducir otra lista de numeros? 'si' o 'no': ")
         op = choose.lower()
 
 
+CalNumPares()
